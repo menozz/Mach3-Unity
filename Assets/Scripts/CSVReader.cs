39c0d93acfb66e7eb7f706ad557dea1ecce7452f
Andrey Savelyev <a.savel.vic@gmail.com>
2015-07-03 19:40:49 +0400

    final ver 1.0 with bugs

---
+++ Assets/Scripts/CSVReader.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System.Collections.Generic;
+
+public class CSVReader  {
+
+	List<GoalStr> theGalaxies;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public List<GoalStr> getList ()
+	{
+		theGalaxies = new List<GoalStr>();
+//		if(File.OpenRead(@"C:\test.csv").CanRead){Debug.Log("true");}
+//		else{ Debug.Log("false");};
+
+		StreamReader reader = new StreamReader(File.OpenRead(@"C:\test.csv"));
+		List<string> listA = new List<string>();
+		List<string> listB = new List<string>();
+		while (!reader.EndOfStream)
+		{
+			string line = reader.ReadLine();
+			string[] values = line.Split(';');
+//			Debug.Log(line);
+			listA.Add(values[0]);Debug.Log(values[0]);
+			listB.Add(values[1]);Debug.Log(values[1]);
+			theGalaxies.Add(new GoalStr(){Date_ = values[0],Goals_=int.Parse(values[1])});
+		}
+
+		theGalaxies.Sort(delegate(GoalStr x,GoalStr y)
+		                 {
+			return y.Goals_.CompareTo(x.Goals_ );
+		});
+
+		return theGalaxies;
+	}
+}
+
+
+
--- Assets/Scripts/RecordsScreen.cs
+++ Assets/Scripts/RecordsScreen.cs
@@ -16,6 +16,7 @@ public class RecordsScreen : MonoBehaviour {
 	GameObject canv;
 
 	LoadRecords lrec;
+	CSVReader csvread;
 
 	// Use this for initialization
 	void Start () {
@@ -24,24 +25,44 @@ public class RecordsScreen : MonoBehaviour {
 
 		GameObject hRecord;
 		canv = GameObject.Find ("Canvas1");
-		lrec = new LoadRecords();
-		//theGalaxies = this.GetComponent<LoadRecords> ().getList ();
-		theGalaxies = lrec.getList ();
 
-		int j = 0;
-		while(PlayerPrefs.HasKey("game_" + j))
-		{
-			hRecord =(GameObject) Instantiate(hScoreField, new Vector2(startX, startY+y), Quaternion.identity); 
-			hRecord.transform.SetParent(canv.transform,false);
-			hRecord.transform.GetChild(0).GetComponent<Text>().text = theGalaxies[j].Date_;
-			hRecord.transform.GetChild(1).GetComponent<Text>().text = theGalaxies[j].Goals_+"";
-			if(j==0 && ScoresManager.Instance.showNewScrorec ){
-			   hRecord.transform.GetComponent<FlashText>().flash();
-			   ScoresManager.Instance.showNewScrorec = false;
+
+		//theGalaxies = this.GetComponent<LoadRecords> ().getList ();
+		if(!PlayerPrefs.HasKey("game_0")){
+			Debug.Log("load from csv");
+			csvread = new CSVReader ();
+			theGalaxies = csvread.getList();
+
+			foreach(GoalStr fvar in theGalaxies)
+			{
+				hRecord =(GameObject) Instantiate(hScoreField, new Vector2(startX, startY+y), Quaternion.identity); 
+				hRecord.transform.SetParent(canv.transform,false);
+				hRecord.transform.GetChild(0).GetComponent<Text>().text = fvar.Date_;
+				hRecord.transform.GetChild(1).GetComponent<Text>().text = fvar.Goals_+"";
+			
+				y=y+shagY;
 			}
-			y=y+shagY;
-			j++;
-		}
+		}else {
+			lrec = new LoadRecords();
+			theGalaxies = lrec.getList ();
+			int j = 0;
+			while(PlayerPrefs.HasKey("game_" + j))
+			{
+				hRecord =(GameObject) Instantiate(hScoreField, new Vector2(startX, startY+y), Quaternion.identity); 
+				hRecord.transform.SetParent(canv.transform,false);
+				hRecord.transform.GetChild(0).GetComponent<Text>().text = theGalaxies[j].Date_;
+				hRecord.transform.GetChild(1).GetComponent<Text>().text = theGalaxies[j].Goals_+"";
+				if(j==0 && ScoresManager.Instance.showNewScrorec ){
+					hRecord.transform.GetComponent<FlashText>().flash();
+					ScoresManager.Instance.showNewScrorec = false;
+				}
+				y=y+shagY;
+				j++;
+			}
+		
+		};
+
+
 //
 //		if (ScoresManager.Instance.showNewScrorec) {
 //			hRecord.transform.GetComponent<FlashText>().flash();
